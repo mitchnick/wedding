d025d71bb41bcda64325925277525a32dd5ba570
Mitchell Nick <mitchnick@gmail.com>
2014-10-23 08:50:36 -0500

    Config try 3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,13 +80,13 @@ Wedding::Application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :user_name => <%= ENV[SENDGRID_USERNAME] %>,
-    :password => <%= ENV[SENDGRID_PASSWORD] %>,
-    :domain => "futurenicks.com",
-    :address => "smtp.sendgrid.net",
-    :port => 587,
-    :authentication => :plain,
-    :enable_starttls_auto => true
+    user_name: "#{ENV[SENDGRID_USERNAME]}",
+    password: "#{ENV[SENDGRID_PASSWORD]}",
+    domain: "futurenicks.com",
+    address: "smtp.sendgrid.net",
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
   }
   config.action_mailer.default_url_options = { host: "futurenicks.com" }
   config.action_mailer.raise_delivery_errors = true
