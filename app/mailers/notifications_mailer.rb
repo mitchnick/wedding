cb73b7b28fcffa74ac67d12a7ed510b2c04ceae0
Mitchell Nick <mitchnick@gmail.com>
2015-04-15 11:42:13 -0500

    Adding in notification emails to mitch and emily when rsvp messages are submitted

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -1,12 +1,22 @@
 class NotificationsMailer < ActionMailer::Base
 
-  default :to => "mitchnick@gmail.com"
+  default :to => ["mitchnick@gmail.com", "emily.budde@gmail.com"]
   default :from => "info@futurenicks.com"
 
   def contact_us(message)
     @message = message
     mail( :subject => "New Message received from #{message.name}",
           :from => message.email)
+  end
+
+  def rsvp_created(rsvp_id)
+    @rsvp = Rsvp.find(rsvp_id)
+    mail( :submit => "Futurenicks: New RSVP Submitted")
+  end
+
+  def rsvp_updated(rsvp_id)
+    @rsvp = Rsvp.find(rsvp_id)
+    mail( :submit => "Futurenicks: RSVP Updated")
   end
 
 end
